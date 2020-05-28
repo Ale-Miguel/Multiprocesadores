d9db514345525b6d28635df0733db6786dd34386
Ale-Miguel <alemiguel97@hotmail.com>
2020-05-28 18:30:10 -0500

    Implemented matrix mult seq and reading from file

--- ProyectoFinal/MatrixMult.cpp
+++ ProyectoFinal/MatrixMult.cpp
@@ -3,7 +3,7 @@ Programa Final - Multiplicación de Matrices
 
 Por:
 Alejandro Miguel Sánchez Mora - A01272385
-Jessica Tovar
+Jessica Tovar Saucedo - A00818101
 
 Procesamiento:
 	• El programa hará 15 veces el cálculo de la multiplicación de la matriz A x matriz B.
@@ -38,17 +38,81 @@ Validaciones:
 
 using namespace std;
 
-#define MATRIX_A_FILE_NAME "test.txt"
-#define MATRIX_B_FILE_NAME "test.txt"
-#define MATRIX_C_FILE_NAME "test.txt"
+#define MATRIX_A_FILE_NAME "matrixA.txt"
+#define MATRIX_B_FILE_NAME "matrixB.txt"
+#define MATRIX_C_FILE_NAME "matrixC.txt"
 
 double **matrixA;
 double **matrixB;
-double **matrixC;
+double **matrixCseq;		//Apuntador que guarda la matriz resultante del código secuencial
+double **matrixCparalel;	//Apuntador que guarda la matriz resultante del código en paralelo
 
 int columnsA, rowsA;
 int columnsB, rowsB;
 int columnsC, rowsC;
+
+//Función para estandarizar el formato de mensaje de error
+void sendErrorMessage(string message) {
+	cout << "ERROR: " << message << endl;
+	exit(0);
+}
+
+void sequentialCode(double**& matrix) {
+
+	matrix = (double**)malloc(rowsC * sizeof(double*));
+	if (!matrix) {
+		sendErrorMessage("No se pudo asignar memoria para matriz C en código secuencial");
+	}
+
+	for (int i = 0; i < rowsC; i++) {
+		matrix[i] = (double*)malloc(columnsC * sizeof(double));
+
+		if (!matrix[i]) {
+			sendErrorMessage("No se pudo asignar memoria para matriz C en código secuencial");
+		}
+		for (int j = 0; j < columnsC; j++) {
+			matrix[i][j] = 0;
+		}
+	}
+
+	for (int i = 0; i < rowsC; i++) {
+		for (int j = 0; j < columnsC; j++) {
+			for (int k = 0; k < rowsB; k++) {
+				matrix[i][j] += matrixA[i][k] * matrixB[k][j];
+			}
+		}
+	}
+}
+
+//Función para llenar las matices cuya información se obtiene de un archivo
+void fillMatrix(double** &matrix, ifstream &matrixFile, int rows, int columns) {
+	matrix = (double**)malloc(rows * sizeof(double*));
+
+	if (!matrix) {
+		sendErrorMessage("No se pudo asignar la memoria suficiente para la matriz");
+	}
+
+	double line;	//Variable que va guardando el valor de cada línea del archivo
+
+	for (int i = 0; i < rows; i++) {
+
+		matrix[i] = (double*)malloc(columns * sizeof(double));
+
+		if (!matrix[i]) {
+			sendErrorMessage("No se pudo asignar la memoria suficiente para la matriz");
+		}
+
+		for (int j = 0; j < columns; j++) {
+
+			if (matrixFile >> line) {
+				matrix[i][j] = line;
+			}
+			else {
+				sendErrorMessage("Elementos en archivo no suficientes para llenar la matriz");
+			}
+		}
+	}
+}
 
 
 int main() {
@@ -60,8 +124,7 @@ int main() {
 
 	//Se verifica que los archivos se puedan abrir
 	if (!matrixAFile.is_open() || !matrixBFile.is_open() || !matrixCFile.is_open()) {
-		cout << "ERROR: Could not open file" << endl;
-		return 0;
+		sendErrorMessage("No se pudo abrir el archivo");
 	}
 
 	cout << "Cantidad de filas de Matriz A: ";
@@ -78,13 +141,51 @@ int main() {
 
 	//Se valida que la multiplicación se pueda hacer
 	if (rowsA != columnsB) {
-		cout << "ERROR: can't do the matrix multiplications (rows_matrix_A != columns_matrix_B)" << endl;
-		return 0;
+		sendErrorMessage("No se puede hacer la multiplicacion de matrices");
+	}
+	
+	rowsC = rowsA;
+	columnsC = columnsB;
+
+	fillMatrix(matrixA, matrixAFile, rowsA, columnsA);
+	fillMatrix(matrixB, matrixBFile, rowsB, columnsB);
+
+	sequentialCode(matrixCseq);
+
+	cout << "Matrix A" << endl;
+	for (int i = 0; i < rowsA; i++) {
+		for (int j = 0; j < columnsA; j++) {
+			cout << matrixA[i][j] << "\t";
+		}
+
+		cout << endl;
 	}
 
+	cout << "Matrix B" << endl;
+	for (int i = 0; i < rowsB; i++) {
+		for (int j = 0; j < columnsB; j++) {
+			cout << matrixB[i][j] << "\t";
+		}
 
+		cout << endl;
+	}
 
+	cout << "Matrix C sequential" << endl;
+	for (int i = 0; i < rowsC; i++) {
+		for (int j = 0; j < columnsC; j++) {
+			cout << matrixCseq[i][j] << "\t";
+		}
 
+		cout << endl;
+	}
+
+	matrixAFile.close();
+	matrixBFile.close();
+	matrixCFile.close();
+
+	free(matrixA);
+	free(matrixB);
+	free(matrixCseq);
 	
 	return 0;
 }
